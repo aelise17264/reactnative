0d441637e47a9a1f99d4ea60b463597001ca8a53
unknown <aelise17264@gmail.com>
2021-06-15 15:19:38 -0600

    added another piece of data to each item in our list

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -3,23 +3,32 @@ import {View, Text, StyleSheet, FlatList} from 'react-native';
 
 export default ListScreen = () => {
     const friends =[
-        {name: 'Candice'},
-        {name: 'Ian'},
-        {name: 'Maddie'},
-        {name: 'Becca'},
-        {name: 'Hannah'},
+        {name: 'Candice', age: '30'},
+        {name: 'Ian', age: '32'},
+        {name: 'Maddie', age: '31'},
+        {name: 'Becca', age: '33'},
+        {name: 'Hannah', age: '28'},
     ]
 
     return (
     <FlatList 
+        //horizontal //tells our list that we want our list to scroll left to right instead of up to down, a scrollbar will automatically generate
+        //showsHorizontalScrollIndicator={false}//gets rid of the automatic scrollbar
         keyExtractor={(friend) => friend.name}
         data={friends} 
         renderItem={({item}) => { //destructured prop to directly reference each item of the element(list)
-            return <Text>{item.name}</Text>
+            return <Text style={styles.textStyle}>
+            {item.name} - Age {item.age}
+            </Text>
 
     }} />
     )
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    textStyle: {
+        marginVertical: 50
+    }
+
+});
 
