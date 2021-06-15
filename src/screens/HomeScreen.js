f898815f9d16b2aecc0b95aa5513161cf6cb8cf9
unknown <aelise17264@gmail.com>
2021-06-15 15:33:00 -0600

    added a touchable opacity to compare to regular button

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,18 @@
 import React from "react";
-import { Text, StyleSheet, View, Button } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
 const HomeScreen = () => {
   return (
     <View>
-    <Text style={styles.text}>It's Summer</Text> 
-    <Button 
-      title="Go to Components Demo"
-      onPress={() => console.log('Button pressed')}
-      />
+      <Text style={styles.text}>It's Summer</Text> 
+      <Button 
+        title="Go to Components Demo"
+        onPress={() => console.log('Button pressed')}
+        />
+      <TouchableOpacity onPress={() => console.log('List Pressed')}>
+        <Text>Go to List Demo</Text> 
+        {/* can put as many blocks of text inside a TouchableOpacity */}
+      </TouchableOpacity>
     </View>
   )
 };
