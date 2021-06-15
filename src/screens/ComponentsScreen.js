0713f8ede2e2084b597036f03a4c7b530a0903bd
unknown <aelise17264@gmail.com>
2021-06-15 14:17:05 -0600

    updated ComponentsScreen to fulfill challenge to add specific styles to individual elements

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -2,18 +2,24 @@ import React from 'react';
 import { Text, StyleSheet, View } from 'react-native';
 
 export default ComponentsScreen = () => {
-    const greeting = <Text>Welcome to the App!</Text>
+    const yourname = <Text style={styles.nameStyle}>My name is Anna</Text>
     return (
-    <View>
-        <Text style={styles.textStyle}>This is the ComponentsScreen</Text>
-        {greeting}
-    </View>
+        <View>
+            <Text style={styles.headingStyle}>Getting started with React Native!</Text>
+            {yourname}
+        </View>
     )
 };
 
 const styles = StyleSheet.create({
     textStyle: {
         fontSize: 30,
+    },
+    headingStyle:{
+        fontSize: 45,
+    },
+    nameStyle:{
+        fontSize: 20
     }
 
 })
