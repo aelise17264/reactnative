12bdf2d1943bcd640120e7a224c535294077daac
unknown <aelise17264@gmail.com>
2021-06-15 15:28:39 -0600

    added button to homescreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,16 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button } from "react-native";
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>It's Summer</Text>;
+  return (
+    <View>
+    <Text style={styles.text}>It's Summer</Text> 
+    <Button 
+      title="Go to Components Demo"
+      onPress={() => console.log('Button pressed')}
+      />
+    </View>
+  )
 };
 
 const styles = StyleSheet.create({
