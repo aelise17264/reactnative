14355235aad2d9a2486c47bc95f81376290327f4
unknown <aelise17264@gmail.com>
2021-06-16 16:20:55 -0600

    added counter function to counter screen

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Text, StyleSheet, View, Button } from 'react-native';
 
 export default CounterScreen = () => {
-    return <Text>This is the counter screen</Text>
+    const [counter, setCounter] = useState(0);
+    //we don't modify state variables directly
 
+    return (
+    <View>
+        <Text>This is the counter screen</Text>
+        <Button title="+" onPress={() =>{
+            setCounter(counter + 1)
+        }}
+         />
+        <Button title="-" onPress={() => {
+            setCounter(counter - 1)
+        }}
+        />
+    </View>
+)
 };
 
 const styles = StyleSheet.create({
