df5856fdd46320c686f9c1e687549b13e3ebf38f
unknown <aelise17264@gmail.com>
2021-06-16 18:33:05 -0600

    connected color screen with home screen and app, added logic to generate random colors with rgb values

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -2,12 +2,23 @@ import React, {useState} from 'react';
 import { StyleSheet, View, Button } from 'react-native';
 
 export default ColorScreen = () => {
+
+
     return(
         <View>
             <Button title="Add a Color" />
+            <View style={{height: 100, width: 100, backgroundColor: randomRGB()}} />
         </View>
     )
 };
+
+const randomRGB = () => {
+    const red = Math.floor(Math.random() * 256)
+    const green = Math.floor(Math.random() * 256)
+    const blue = Math.floor(Math.random() * 256)
+
+    return `rgb(${red}, ${green}, ${blue})`
+}
 
 const styles = StyleSheet.create({
 
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -30,6 +30,11 @@ const HomeScreen = ({navigation}) => {
       title="Go to Counter Demo"
       onPress={() => navigation.navigate('Counter')}
       /> 
+      <Button
+      style={styles.button}
+      title="Go to Color Demo"
+      onPress={() => navigation.navigate('Color')}
+      /> 
     </View>
   )
 };
