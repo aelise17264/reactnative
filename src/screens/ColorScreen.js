d05a4e8ee80f4cca528edc23e68ed685eee3ea7b
unknown <aelise17264@gmail.com>
2021-06-16 18:23:43 -0600

    added colorscreen file with boiler plate code

---
+++ src/screens/ColorScreen.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+import { StyleSheet, View, Button } from 'react-native';
+
+export default ColorScreen = () => {
+    return(
+        <View>
+            <Button title="Add a Color" />
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({
+
+})
