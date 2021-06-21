126d48c64ac52e60416745dd38735fd70616d2de
unknown <aelise17264@gmail.com>
2021-06-20 21:01:50 -0600

    updated counter screen to use a reducer

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,26 +1,57 @@
-import React, {useState} from 'react';
+import React, {useReducer} from 'react';
 import { Text, StyleSheet, View, Button } from 'react-native';
 
-export default CounterScreen = () => {
-    const [counter, setCounter] = useState(0);
-    //we don't modify state variables directly
+const reducer = (state, action) => {
+    switch(action.type) {
+        case 'increment' :
+            return {...state, count: state.count + action.payload}
+        case 'decrement':
+            return {...state, count: state.count - action.payload}
+        default:
+            return state;
+    }
+}
 
-    return (
-    <View>
-        <Text>This is the counter screen</Text>
-        <Button title="+" onPress={() =>{
-            setCounter(counter + 1)
+export default CounterScreen = () => {
+    const [state, dispatch] = useReducer(reducer, { count: 0})
+
+    return(
+        <View>
+            <Text>This is the counter screen</Text>
+           <Button title="+" onPress={() =>{
+            dispatch({type: 'increment', paylod: 1})
         }}
          />
         <Button title="-" onPress={() => {
-            setCounter(counter - 1)
+        dispatch({type: 'decrement', paylod: 1})
+
         }}
         />
-        <Text>Current Count: {counter}</Text>
-    </View>
-)
-};
+        <Text>Current Count: {state.count}</Text>
+        </View>
+    )
+}
 
 const styles = StyleSheet.create({
 
 })
+
+// export default CounterScreen = () => {
+//     const [counter, setCounter] = useState(0);
+//     //we don't modify state variables directly
+
+//     return (
+//     <View>
+//         <Text>This is the counter screen</Text>
+//         <Button title="+" onPress={() =>{
+//             setCounter(counter + 1)
+//         }}
+//          />
+//         <Button title="-" onPress={() => {
+//             setCounter(counter - 1)
+//         }}
+//         />
+//         <Text>Current Count: {counter}</Text>
+//     </View>
+// )
+// };
