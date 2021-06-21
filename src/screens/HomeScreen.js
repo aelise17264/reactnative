abb7700c7dc73e8ec0adfbc43af2506bd260a073
unknown <aelise17264@gmail.com>
2021-06-20 21:06:36 -0600

    created TextScreen.js file & connected it to App & home screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -40,6 +40,11 @@ const HomeScreen = ({navigation}) => {
       title="Go to Square Demo"
       onPress={() => navigation.navigate('Square')}
       /> 
+      <Button
+      style={styles.button}
+      title="Go to Text Demo"
+      onPress={() => navigation.navigate('Text')}
+      /> 
     </View>
   )
 };
---
+++ src/screens/TextScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, StyleSheet, View } from 'react-native';
+
+export default Text = () => {
+    return (
+        <View>
+            <Text>Text Screen</Text>
+        </View>
+    )
+};
