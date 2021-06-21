d2ff36fcc460c71eaabebee45c4e7141be86c791
unknown <aelise17264@gmail.com>
2021-06-20 21:52:58 -0600

    set up text input to update state when user enters their name

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,10 +1,28 @@
-import React from 'react';
-import { Text, StyleSheet, View } from 'react-native';
+import React, {useState} from 'react';
+import { Text, StyleSheet, View, TextInput } from 'react-native';
 
-export default Text = () => {
+export default TextScreen = () => {
+    const [name, setName] = useState('');
+
     return (
         <View>
-            <Text>Text Screen</Text>
+            <Text>Enter Your Name:</Text>
+            <TextInput 
+                style={styles.input} 
+                autoCapitalize='none'
+                autoCorrect={false}
+                value={name}
+                onChangeText={(newName) => setName(newName)}
+            />
+            <Text>My name is {name}</Text>
         </View>
     )
 };
+
+const styles = StyleSheet.create({
+    input: {
+        margin: 15,
+        borderColor: 'black',
+        borderWidth: 1
+    }
+})
